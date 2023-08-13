ea4e2035699b58dbc3816e669e53782608ef88cd
cganeshs <chavanganesh229@gmail.com>
2023-08-13 21:18:12 +0530

    allow all host in settings

--- gas_services_backend/settings.py
+++ gas_services_backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-2&vu13o!s1fwo3-p8t%tm2ys+%cs-x(zjh$1259v1zl7+nr68s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
